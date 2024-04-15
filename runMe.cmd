cce249260a12b76f9878d742b38916222eb54410
Jaka Černetič <jaka.cernetic@students.vegova.si>
2024-04-15 22:06:29 +0200

    nova struktura

---
+++ runMe.cmd
@@ -0,0 +1,13 @@
+@echo off
+title Welcome!
+color 0a
+type text.txt
+echo.
+echo.
+choice /C:123 /N /M "..."
+if errorlevel 1 goto virus
+if errorlevel 2 goto game   
+if errorlevel 3 goto about
+
+:VIRUS
+start virus\main.cmd
