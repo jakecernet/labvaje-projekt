bbf8ec54be47346616aaed435df9574aa80e573f
jakecernet <jaka.cernetic@students.vegova.si>
2024-04-22 19:59:31 +0200

    ruska ruleta

---
+++ games/russian.cmd
@@ -0,0 +1,24 @@
+@echo off
+color 0a
+title Russian Roulette
+echo.
+echo.
+echo.                              I want to play a game...
+echo.
+echo.
+echo.                                  Russian Roulette
+echo.
+echo.                               You have 1 in 6 chance
+echo.                               that your files will be
+echo.                               encrypted. If you lose,
+echo.                               you will have to pay
+echo.                               1000$ to get them back.
+echo.
+echo.                               If you win, a secret
+echo.                            power mode will be unlocked.
+echo.
+echo.                             Press any key to continue...
+pause >nul
+echo.
+echo.
+echo.                                     Loading...
