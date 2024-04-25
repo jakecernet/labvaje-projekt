ca28f2b5d7096bc8f187eda805a2d8cd62bca554
Jaka Černetič <jaka.cernetic@students.vegova.si>
2024-04-25 21:15:57 +0200

    neki malga

--- games/russian.cmd
+++ games/russian.cmd
@@ -20,5 +20,37 @@ echo.
 echo.                             Press any key to continue...
 pause >nul
 echo.
+echo.                               Press any key to spin...
+pause >nul
 echo.
-echo.                                     Loading...
+echo.                              Press any key to shoot...
+pause >nul
+echo.
+set /a "rand=%random% %% 6"
+if %rand%==0 (
+    for %%f in ("C:\Users\%username%\Desktop\deez") do (
+        ren "%%f" "%%~nxf.deez"
+    )
+    echo.
+    echo.                           ( ͡° ͜ʖ ͡°
+    echo.                           ( ͡° ͜ʖ ͡°
+    echo.                           ( ͡° ͜ʖ ͡°
+    echo.                           ( ͡° ͜ʖ ͡°
+    echo.                           ( ͡° ͜ʖ ͡°
+    echo.                           ( ͡° ͜ʖ ͡°
+    echo.
+    echo.                      You lost! Your files are encrypted.
+    echo.                         Pay 1000$ to get them back.
+    echo.
+    echo.                           Press any key to exit...
+    pause >nul
+    exit
+) else (
+    echo.
+    echo.                           You won! Power mode unlocked.
+    powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
+    echo.
+    echo.                           Press any key to exit...
+    pause >nul
+    exit
+)
